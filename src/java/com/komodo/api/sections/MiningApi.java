8bc5d9d67ad9afa90d558dc3b4c01a5da241b5d8
Giffy Chris <chrisgiffy@gmail.com>
2019-06-19 13:36:03 -0500

    completed mining block

--- src/java/com/komodo/api/sections/MiningApi.java
+++ src/java/com/komodo/api/sections/MiningApi.java
@@ -45,33 +45,61 @@ public class MiningApi {
 		return output;
 	}
 	
-	public String getLocalSolps(Configurations config, String hex) {
-		String output = KomodoUtil.fireKomodo(config, StringConstants.GET_LOCAL_SOLPS, "\""+hex+"\"");
+	public String getBlockTemplate(Configurations config, String mode, List<String> capabilities) {
+		StringBuilder params = new StringBuilder("{\"mode\":\"");
+		params.append(mode);
+		params.append("\",");
+		params.append("\"capabilities\":[");
+		for(int i = 0; i < capabilities.size(); i++) {
+			if(i < 0) {
+				params.append(",");
+			}
+			params.append("\"");
+			params.append(capabilities.get(i));
+			params.append("\"");
+		}
+		params.append("]}");
+		String output = KomodoUtil.fireKomodo(config, StringConstants.GET_BLOCK_TEMPLATE, params.toString());
 		return output;
 	}
 	
-	public String getMiningInfo(Configurations config, String hex) {
-		String output = KomodoUtil.fireKomodo(config, StringConstants.GET_MINING_INFO, "\""+hex+"\"");
+	public String getLocalSolps(Configurations config) {
+		String output = KomodoUtil.fireKomodo(config, StringConstants.GET_LOCAL_SOLPS, null);
 		return output;
 	}
 	
-	public String getNetworkHashPs(Configurations config, String hex) {
-		String output = KomodoUtil.fireKomodo(config, StringConstants.GET_NETWORK_HASH_PS, "\""+hex+"\"");
+	public String getMiningInfo(Configurations config) {
+		String output = KomodoUtil.fireKomodo(config, StringConstants.GET_MINING_INFO, null);
 		return output;
 	}
 	
-	public String getNetworkSolps(Configurations config, String hex) {
-		String output = KomodoUtil.fireKomodo(config, StringConstants.GET_NETWORK_SOLPS, "\""+hex+"\"");
+	public String getNetworkHashPs(Configurations config) {
+		String output = KomodoUtil.fireKomodo(config, StringConstants.GET_NETWORK_HASH_PS, null);
 		return output;
 	}
 	
-	public String prioritiseTransaction(Configurations config, String hex) {
-		String output = KomodoUtil.fireKomodo(config, StringConstants.PRIORITISE_TRANSACTION, "\""+hex+"\"");
+	public String getNetworkHashPs(Configurations config, int blocks, int height) {
+		String output = KomodoUtil.fireKomodo(config, StringConstants.GET_NETWORK_HASH_PS, blocks+","+height);
 		return output;
 	}
 	
-	public String submitBlock(Configurations config, String hex) {
-		String output = KomodoUtil.fireKomodo(config, StringConstants.SUBMIT_BLOCK, "\""+hex+"\"");
+	public String getNetworkSolps(Configurations config) {
+		String output = KomodoUtil.fireKomodo(config, StringConstants.GET_NETWORK_SOLPS, null);
+		return output;
+	}
+	
+	public String getNetworkSolps(Configurations config, int blocks, int height) {
+		String output = KomodoUtil.fireKomodo(config, StringConstants.GET_NETWORK_SOLPS, blocks+","+height);
+		return output;
+	}
+	
+	public String prioritiseTransaction(Configurations config, String txId, double priorityDelta, double feeDelta) {
+		String output = KomodoUtil.fireKomodo(config, StringConstants.PRIORITISE_TRANSACTION, "\""+txId+"\","+priorityDelta+","+feeDelta);
+		return output;
+	}
+	
+	public String submitBlock(Configurations config, String hexData ) {
+		String output = KomodoUtil.fireKomodo(config, StringConstants.SUBMIT_BLOCK, "\""+hexData+"\"");
 		return output;
 	}
 }
